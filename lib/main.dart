81a68165e1e6acdc2ef6e7ec1caf7dff3d931ac0
Hasnen tai <hasnentai@Hasnens-MacBook.local>
2018-09-24 00:31:20 +0530

    homepage

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class QuickBee extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quick Bee',
+      debugShowCheckedModeBanner: false,
       // Set Raleway as the default app font
       theme: ThemeData(fontFamily: 'Roboto'),
       home: MyHomePage(),
@@ -17,6 +18,121 @@ class QuickBee extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return new Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Stack(
+              alignment: Alignment.center,
+              children: <Widget>[
+                new Container(
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFF18D191)),
+                  child: new Icon(
+                    Icons.local_offer,
+                    color: Colors.white,
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(right: 50.0, top: 50.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFFFC6A7F)),
+                  child: new Icon(
+                    Icons.home,
+                    color: Colors.white,
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(left: 30.0, top: 50.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFFFFCE56)),
+                  child: new Icon(
+                    Icons.local_car_wash,
+                    color: Colors.white,
+                  ),
+                ),
+                new Container(
+                  margin: new EdgeInsets.only(left: 90.0, top: 40.0),
+                  height: 60.0,
+                  width: 60.0,
+                  decoration: new BoxDecoration(
+                      borderRadius: new BorderRadius.circular(50.0),
+                      color: Color(0xFF45E0EC)),
+                  child: new Icon(
+                    Icons.place,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top:8.0,bottom: 80.0),
+                  child: new Text(
+                    "Quick Bee",
+                    style: new TextStyle(fontSize: 30.0),
+                  ),
+                )
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0,right: 20.0,top: 10.0),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      height: 60.0,
+                      decoration: new BoxDecoration(color: Color(0xFF18D191),borderRadius: new BorderRadius.circular(10.0)),
+                      child: new Text("Sign In With Email",style: new TextStyle(fontSize: 20.0,color: Colors.white))
+                    ),
+                  ),
+                )
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0,right: 5.0,top: 10.0),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      height: 60.0,
+                      decoration: new BoxDecoration(color: Color(0xFF4364A1),borderRadius: new BorderRadius.circular(10.0)),
+                      child: new Text("FaceBook",style: new TextStyle(fontSize: 20.0,color: Colors.white))
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10.0,right: 20.0,top: 10.0),
+                    child: new Container(
+                      alignment: Alignment.center,
+                      height: 60.0,
+                      decoration: new BoxDecoration(color: Color(0xFFDF513B),borderRadius: new BorderRadius.circular(10.0)),
+                      child: new Text("Google",style: new TextStyle(fontSize: 20.0,color: Colors.white))
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
